98e21bc363ba04a5f5dc0f4ea61963cea3ecd1fe
DeepCFoxXx <learncode23@gmail.com>
2018-03-27 15:18:22 +0100

    save added

---
+++ models/member.rb
@@ -0,0 +1,35 @@
+require_relative('../db/sql_runner.rb')
+
+class Member
+
+  attr_accessor :age, :first_name, :last_name, :house_id
+  attr_reader :id
+
+  def initialize(options)
+    @id = options['id'].to_i if options['id']
+    @first_name = options['first_name']
+    @last_name = options['last_name']
+    @house_id = options['house_id'].to_i
+    @age = options['age'].to_i
+  end
+
+  def save()
+    sql = "INSERT INTO members
+    (
+      first_name,
+      last_name,
+      house_id,
+      age
+    )
+    VALUES
+    (
+      $1, $2, $3, $4
+    )
+    RETURNING id"
+    values = [@first_name, @last_name, @house_id, @age]
+    result = SqlRunner.run(sql, values)
+    id = result.first['id']
+    @id = id
+  end
+
+end
