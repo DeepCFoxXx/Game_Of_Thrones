b5438cda8ec183c1529d384372ed5e68d2f752ae
DeepCFoxXx <learncode23@gmail.com>
2018-03-28 16:13:18 +0100

    added more paths to controller

--- controller.rb
+++ controller.rb
@@ -18,8 +18,25 @@ post '/members' do
   redirect to '/members'
 end
 
+get '/members/:id' do
+  @member = Member.find(params['id'])
+  erb(:show)
+end
+
 get '/members/:id/edit' do
   @houses = House.all
   @member = Member.find(params['id'])
   erb(:edit)
+end
+
+post '/members/:id' do
+  member = Member.new(params)
+  member.update
+  redirect to "/members/#{params['id']}"
+end
+
+post '/members/:id/delete' do
+  member = Member.find(params['id'])
+  member.delete
+  redirect to '/members'
 end
